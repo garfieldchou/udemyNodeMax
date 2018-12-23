48e4e389e48719140da444fa96cd2242c744fbc1
garfield <cmgarfield@gmail.com>
2018-12-23 22:09:17 +0800

    Use getProducts() method to fetch related products
    req.user has getProducts() method because of association
    
    Lec.165, Fetching Related Products

--- sec11-sequelize/01-defining-a-model/controllers/admin.js
+++ sec11-sequelize/01-defining-a-model/controllers/admin.js
@@ -28,8 +28,14 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findByPk(prodId)
-    .then(product => {
+  req.user
+    .getProducts({ where: { id: prodId }})
+    // Product.findByPk(prodId)
+    .then(products => {
+      const product = products[0];
+      if (!product) {
+        return res.redirect('/');
+      }
       res.render('admin/edit-product', {
         pageTitle: 'Edit Product',
         path: '/admin/edit-product',
@@ -54,7 +60,8 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+  req.user
+    .getProducts()
     .then(products => {
       res.render('admin/products', {
         prods: products,
