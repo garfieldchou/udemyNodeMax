25589634b8d68ed3e5f9eef6c6200e39e0890aa9
garfield <cmgarfield@gmail.com>
2019-01-02 16:23:12 +0800

    Add the getDb() function to get the db instance
    First, use mongoConnect() to create the db connection and save the db
    instance to a local _db variable.
    Second, create getDb() function to get the db instance.
    
    Lec.178, Finishing the Database Connection

--- sec12-mongodb/00-starting-setup/util/database.js
+++ sec12-mongodb/00-starting-setup/util/database.js
@@ -1,15 +1,27 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
+let _db;
+
 const mongoConnect = callback => {
-  MongoClient.connect('mongodb+srv://gc:<PASSWORD>@cluster0-f5nti.mongodb.net/test?retryWrites=true', { useNewUrlParser: true })
+  MongoClient.connect('mongodb+srv://gc:<PASSWORD>@cluster0-f5nti.mongodb.net/shop?retryWrites=true', { useNewUrlParser: true })
     .then(client => {
       console.log('Connected!');
-      callback(client);
+      _db = client.db();
+      callback();
     })
     .catch(err => {
       console.log(err);
+      throw err;
     });
 };
 
-module.exports = mongoConnect;
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw 'No database found!';
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
