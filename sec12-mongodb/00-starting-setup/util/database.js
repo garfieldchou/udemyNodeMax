e54d70246aeba533f68987eb0a943c9e2f631ae0
garfield <cmgarfield@gmail.com>
2019-01-02 15:25:20 +0800

    Install mongodb driver and test connection
    First, npm install mongodb.
    Second, remove sequelize code in app.js.
    Third, connect to the database and print out the connected client
    instance.
    
    Lec.176, Installing the MongoDB Driver

--- sec12-mongodb/00-starting-setup/util/database.js
+++ sec12-mongodb/00-starting-setup/util/database.js
@@ -1,8 +1,15 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+const mongoConnect = callback => {
+  MongoClient.connect('mongodb+srv://gc:<PASSWORD>@cluster0-f5nti.mongodb.net/test?retryWrites=true', { useNewUrlParser: true })
+    .then(client => {
+      console.log('Connected!');
+      callback(client);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
 
-module.exports = sequelize;
+module.exports = mongoConnect;
