44b94ad00c86d3b15bce18a40914b5e9bdaf897d
garfield <cmgarfield@gmail.com>
2019-01-03 10:13:46 +0800

    Get the details of a single product
    First, pass ObjectId instead of id string to find the product.
    Second, use next() to get the next available document from the cursor.
    
    Lec.183, Fetching a Single Product

--- sec12-mongodb/00-starting-setup/models/product.js
+++ sec12-mongodb/00-starting-setup/models/product.js
@@ -1,5 +1,5 @@
+const mongodb = require('mongodb');
 const getDb = require('../util/database').getDb;
-
 class Product {
   constructor(title, price, description, imageUrl) {
     this.title = title;
@@ -32,6 +32,20 @@ class Product {
       .catch(err => {
         console.log(err);
       });
+  }
+
+  static findById(prodId) {
+    const db = getDb();
+    return db.collection('products')
+      .find({ _id: new mongodb.ObjectId(prodId) })
+      .next()
+      .then(product => {
+        console.log(product);
+        return product;
+      })
+      .catch(err => {
+        console.log(err);
+      })
   }
 }
 
