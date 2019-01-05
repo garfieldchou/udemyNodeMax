54b64cd004ec441a1789f1d9da116a1bbca97c4c
garfield <cmgarfield@gmail.com>
2019-01-05 20:09:49 +0800

    Display orders
    Find nested fields and apply 'project' method to the returned cursor.
    
    Lec.200, Getting Orders

--- sec12-mongodb/00-starting-setup/models/user.js
+++ sec12-mongodb/00-starting-setup/models/user.js
@@ -100,6 +100,15 @@ class User {
       });
   }
 
+  getOrders() {
+    const db = getDb();
+    return db
+      .collection('orders')
+      .find({ 'user._id': new ObjectId(this._id) })
+      .project({ items: 1 })
+      .toArray();
+  }
+
   static findById(userId) {
     const db = getDb();
     return db.collection('users').findOne({ _id: new ObjectId(userId) });
