eddcdfbdfc73c807790559b7fe136abef1bda994
garfield <cmgarfield@gmail.com>
2019-01-10 22:49:03 +0800

    Create the 'Product' model and save products
    First, use the schema of product and export mongoose.model().
    Second, save products in admin controller.
    
    Lec.209, Saving Data Through Mongoose

--- practice/controllers/admin.js
+++ practice/controllers/admin.js
@@ -9,11 +9,8 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
-  const title = req.body.title;
-  const imageUrl = req.body.imageUrl;
-  const price = req.body.price;
-  const description = req.body.description;
-  const product = new Product(title, price, description, imageUrl, null, req.user._id);
+  const { title, imageUrl, price, description } = req.body;
+  const product = new Product({ title, imageUrl, price, description });
   product
     .save()
     .then(result => {
