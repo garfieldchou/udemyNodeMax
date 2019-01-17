54b07ac49f33ed3c0b704981064d6e184fead265
garfield <cmgarfield@gmail.com>
2019-01-17 23:02:07 +0800

    Use MongoDB to store Sessions
    First, install 'connect-mongodb-session' package.
    Second, require 'connect-mongodb-session' package and create
    MongoDBStore constructor.
    Third, create MongoDBStore instance with uri and collection setting.
    Fourth, pass the store instance to session middleware option.
    
    Lec.237, Using MongoDB to Store Sessions

--- practice/app.js
+++ practice/app.js
@@ -4,11 +4,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
+const MONGODB_URI = 'mongodb+srv://gc:<PASSWORD>@cluster0-f5nti.mongodb.net/shop?retryWrites=true';
+
 const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions'
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -19,11 +26,14 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({
-  secret: 'my secret',
-  resave: false,
-  saveUninitialized: false
-}));
+app.use(
+  session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  })
+);
 
 app.use((req, res, next) => {
   User.findById("5c3bf5f82d10ab3cf8b28851")
@@ -42,7 +52,7 @@ app.use(errorController.get404);
 
 mongoose
   .connect(
-    'mongodb+srv://gc:<PASSWORD>@cluster0-f5nti.mongodb.net/shop?retryWrites=true',
+    MONGODB_URI,
     { useNewUrlParser: true }
   )
   .then(result => {
