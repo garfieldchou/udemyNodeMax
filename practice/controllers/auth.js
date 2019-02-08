5e2ff0dc4c44b4ea57582a92831e9f21d7d05b23
garfield <cmgarfield@gmail.com>
2019-02-08 21:47:10 +0800

    Validate email field with express-validator
    First, use check API to check email input field.
    Second, use validation result API to retrieve the validation errors.
    
    Lec.287, Setup & Basic Validation

--- practice/controllers/auth.js
+++ practice/controllers/auth.js
@@ -3,6 +3,7 @@ const crypto = require('crypto');
 const bcrypt = require('bcryptjs');
 const nodemailer = require('nodemailer');
 const sendgridTransport = require('nodemailer-sendgrid-transport');
+const { validationResult } = require('express-validator/check');
 
 const User = require('../models/user');
 
@@ -72,6 +73,15 @@ exports.postLogin = (req, res, next) => {
 
 exports.postSignup = (req, res, next) => {
   const { email, password, confirmPassword } = req.body;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log(errors.array());
+    return res.status(422).render('auth/signup', {
+      path: '/signup',
+      pageTitle: 'Signup',
+      errorMessage: errors.array()
+    });
+  }
   User.findOne({ email })
     .then(userDoc => {
       if (userDoc) {
