3d659c2a2109ccaa84b04ddc1a4f0ea3a3d55911
garfield <cmgarfield@gmail.com>
2019-02-15 10:38:40 +0800

    Show the detailed validation errors in the view
    First, pass the validation errors array in res.render().
    Second, update the view to render the errors.
    
    Lec.298, Validating Product Editing

--- practice/controllers/admin.js
+++ practice/controllers/admin.js
@@ -8,7 +8,8 @@ exports.getAddProduct = (req, res, next) => {
     path: '/admin/add-product',
     editing: false,
     hasError: false,
-    errorMessage: null
+    errorMessage: null,
+    validationErrors: []
   });
 };
 
@@ -28,7 +29,8 @@ exports.postAddProduct = (req, res, next) => {
       editing: false,
       hasError: true,
       product: { title, imageUrl, price, description },
-      errorMessage: errors.array()[0].msg
+      errorMessage: errors.array()[0].msg,
+      validationErrors: errors.array()
     });
   }
   const product = new Product({
@@ -67,7 +69,8 @@ exports.getEditProduct = (req, res, next) => {
         editing: editMode,
         product: product,
         hasError: false,
-        errorMessage: null
+        errorMessage: null,
+        validationErrors: []
       });
     })
     .catch(err => console.log(err));
@@ -79,6 +82,26 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
+
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).render('admin/edit-product', {
+      pageTitle: 'Edit Product',
+      path: '/admin/edit-product',
+      editing: true,
+      hasError: true,
+      product: {
+        title: updatedTitle,
+        imageUrl: updatedImageUrl,
+        price: updatedPrice,
+        description: updatedDesc,
+        _id: prodId
+      },
+      errorMessage: errors.array()[0].msg,
+      validationErrors: errors.array()
+    });
+  }
 
   Product.findById(prodId)
     .then(product => {
