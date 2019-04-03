fc9a512396aff0b611fc97c9bfa3295bef77ae81
garfield <cmgarfield@gmail.com>
2019-04-03 23:27:10 +0800

    Set up csrf token before rendering nav items
    Set up csrf token before rendering nav items to avoid rendering error.
    
    Lec.309, Using the Error Handling Middleware Correctly

--- practice/app.js
+++ practice/app.js
@@ -43,6 +43,12 @@ app.use(csrfProtection);
 app.use(flash());
 
 app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
+
+app.use((req, res, next) => {
   throw new Error('Sync Dummy');
   if (!req.session.user) {
     return next();
@@ -58,12 +64,6 @@ app.use((req, res, next) => {
     .catch(err => {
       throw new Error(err);
     });
-});
-
-app.use((req, res, next) => {
-  res.locals.isAuthenticated = req.session.isLoggedIn;
-  res.locals.csrfToken = req.csrfToken();
-  next();
 });
 
 app.use('/admin', adminRoutes);
