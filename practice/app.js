045edb4f0a88a420bae929c0f7fdf598259e1645
garfield <cmgarfield@gmail.com>
2019-04-03 23:02:52 +0800

    Render error page for express error handling
    Render error page instead of redirecting '/500' for error handling to
    avoid infinite loops.
    
    Lec.309, Using the Error Handling Middleware Correctly

--- practice/app.js
+++ practice/app.js
@@ -43,6 +43,7 @@ app.use(csrfProtection);
 app.use(flash());
 
 app.use((req, res, next) => {
+  throw new Error('Sync Dummy');
   if (!req.session.user) {
     return next();
   }
@@ -75,7 +76,12 @@ app.use(errorController.get404);
 
 app.use((error, req, res, next) => {
   // res.status(error.httpStatusCode).render(...);
-  res.redirect('/500');
+  // res.redirect('/500');
+  res.status(500).render('500', {
+    pageTitle: 'Error!',
+    path: '/500',
+    isAuthenticated: req.session.isLoggedIn
+  });
 });
 
 mongoose
